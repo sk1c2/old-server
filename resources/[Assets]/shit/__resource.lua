1aac2bdcb120f99a45a328a511d9163acfc78944
sk1c2 <70528411+sk1c2@users.noreply.github.com>
2021-07-09 16:35:09 +0100

    Removed something

--- resources/[Assets]/shit/__resource.lua
+++
@@ -1,8 +0,0 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-
---resource_type 'gametype' { name = 'Hot Putsuit' }
-client_script "@prp-errorlog/client/cl_errorlog.lua"
-description 'Fuckhead - PixelRez'
-
-
-client_script "client.lua"
